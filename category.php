c7eaaa3398fbd3e7a8751ec9d27105c3abb6dd69
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-07-15 16:09:45 +0530

    Update category.php

--- category.php
+++ category.php
@@ -4,9 +4,7 @@
     if(!isset($_GET["id"])) {
         ErrorMessage::show("No id passed to page");
     }
-
     $preview = new PreviewProvider($con,$userLoggedIn);
     echo $preview->createCategoryPreviewVideo($_GET["id"]);
-
     $containers = new CategoryContainers($con, $userLoggedIn);
     echo $containers->showMovieCategories($_GET["id"]);
